25dc7a5bc26a5999e1e50c8f0a6cd739bc630bfe
ParkJW <pc224538@gmail.com>
2022-09-23 13:11:48 +0900

    article(call), footer
    article(call), footer
    sidebar : proto

--- js/index.js
+++ js/index.js
@@ -3,17 +3,30 @@
  */
 
 window.onload = function() {
-	const systemImgHover1 = document.querySelector("#system-img-hover1");
-	const systemImgHover2 = document.querySelector("#system-img-hover2");
-	const systemImgHover3 = document.querySelector("#system-img-hover3");
- 
-	const systemImg1 = document.querySelector("#system-img1")
-	const systemImg2 = document.querySelector("#system-img2")
-	const systemImg3 = document.querySelector("#system-img3")
 	
-	const productPopup = document.querySelector("#product-popupdiv");
-	const productButton = document.querySelector("#product-button");
-	const productClose = document.querySelector("#product-popup-close");
+	const menu = document.getElementById("menu");
+	const sidebar = document.getElementById("sidebar");
+	
+	const systemImgHover1 = document.getElementById("system-img-hover1");
+	const systemImgHover2 = document.getElementById("system-img-hover2");
+	const systemImgHover3 = document.getElementById("system-img-hover3");
+ 
+	const systemImg1 = document.getElementById("system-img1");
+	const systemImg2 = document.getElementById("system-img2");
+	const systemImg3 = document.getElementById("system-img3");
+	
+	const productABack = document.getElementById("product-back");
+	const productAFor = document.getElementById("product-forward");
+	const productDiv = document.querySelectorAll(".product-carousel-div");
+	
+	const productPopupDiv = document.getElementById("product-popupdiv");
+	const productButton = document.getElementById("product-button");
+	const productPopup = document.querySelectorAll(".product-popup");
+	const productClose = document.querySelectorAll(".product-popup-close");
+	
+	menu.addEventListener("click", function() {
+		sidebar.style.transform = "translateX(-100%)";
+	})
 	
 	const systemImg1Out = function() {
 		systemImgHover1.style.display = "none";
@@ -58,12 +71,33 @@ window.onload = function() {
 	systemImg3.addEventListener("mouseout", systemImg3Out);
 	systemImgHover3.addEventListener("mouseover", systemImg3Over);	
 	
+	let productIndex = 0;
+	
+	productABack.addEventListener("click", function() {
+		if (productIndex == 0) productIndex = 2;
+		else productIndex--;
+		productDiv.forEach(function(img) {
+			img.style.transform = "translateX(" + productIndex * -800 + "px)"
+		})
+	})
+	
+	productAFor.addEventListener("click", function() {
+		if (productIndex == 2) productIndex = 0;
+		else productIndex++;
+		productDiv.forEach(function(img) {
+			img.style.transform = "translateX(" + productIndex * -800 + "px)"
+		})
+	})
+	
 	productButton.addEventListener("click", function() {
-		productPopup.style.display = "block";
+		productPopupDiv.style.display = "block";
+		productPopup[productIndex].style.display = "flex";
+		productClose[productIndex].addEventListener("click", function(){
+			productPopupDiv.style.display = "none";
+			productPopup[productIndex].style.display = "none";
+		})
 	});
-	productClose.addEventListener("click", function() {
-		productPopup.style.display = "none";
-	});
+	
 }
 
 	
